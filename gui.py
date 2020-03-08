c87ec33dde523d6adce238737a8adce629acf993
tkianai <tkianai@163.com>
2020-03-08 13:56:45 +0800

    add simple gui operation

---
+++ gui.py
@@ -0,0 +1,28 @@
+
+import tkinter as tk
+from core import BackupEngine
+
+
+def callback():
+
+    account = input_text.get()
+    if account == '':
+        print("请先输入QQ账号")
+        return
+    engine = BackupEngine(account, save_dir='./QQZone', headless=True)
+    try:
+        engine.download_images()
+        engine.download_posts()
+    except Exception as e:
+        print(e)
+    finally:
+        engine.finished()
+
+
+window = tk.Tk()
+window.title('QQ空间备份')
+input_text = tk.Entry(window)
+button = tk.Button(window, text='一键备份', command=callback)
+input_text.pack()
+button.pack()
+tk.mainloop()
